6c96b39fe3e05e6559bb4da6e7ac6a538b0d50b1
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-10-24 09:22:28 -0400

    test

---
+++ apcsaconversions.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class apcsarighttriangle {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      final double IN_TO_MI = 63360;
+      final double FEET_TO_MI = 5280;
+      final double YARD_TO_MI = 1760;
+
+      System.out.print("Enter an origin unit: ");
+      char origin = char.nextChar();
+
+      System.out.print("Enter a destination unit: ");
+      char origin = char.nextChar();
+
+
+
+
+
+      in.close();
+    }
+}
